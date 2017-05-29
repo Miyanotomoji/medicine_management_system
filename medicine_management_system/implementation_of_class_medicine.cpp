35f5bc6c2d4eb7b3d3ae0c613c2d0b5bc3d9a003
Miyanotomoji <miyanotomoji@outlook.com>
2017-05-29 16:17:24 +0800

    two new files

---
+++ medicine_management_system/implementation_of_class_medicine.cpp
@@ -0,0 +1,99 @@
+// implementation of class Medicine
+
+#include<string>
+#include<iostream>
+#include<iomanip>
+#include"medicine.h"
+
+using std::string;
+using std::cout;
+using std::endl;
+using std::setw;
+
+int Medicine::count = 0;
+Medicine **Medicine::total_index = new Medicine *[MAX_RECORD];
+
+Medicine::Medicine(int t_number, string t_name, int t_amount, double t_price)
+{
+	number = t_number;
+	name = t_name;
+	amount = t_amount;
+	price = t_price;
+	index = count;
+	accessibility = true;
+	total_index[index] = this;
+	count++;
+}
+
+Medicine::Medicine(Medicine & t_medicine)
+{
+	number = t_medicine.number;
+	name = t_medicine.name;
+	amount = t_medicine.amount;
+	price = t_medicine.price;
+	accessibility = t_medicine.accessibility;
+	count++;
+}
+
+void Medicine::add(int t_number, string t_name, int t_amount, double t_price)
+{
+	number = t_number;
+	name = t_name;
+	amount = t_amount;
+	price = t_price;
+	accessibility = true;
+}
+
+void Medicine::show(int mode = 0)
+{
+	if (mode == 0)
+	{
+		cout << setw(10) << "NO" << setw(10) << "NAME" << setw(10) << "AMOUNT" << setw(10) << "PRICE" << endl;
+		cout << setw(10) << number << setw(10) << name << setw(10) << amount << setw(10) << price << endl;
+	}
+	else
+	{
+		cout << setw(10) << number << setw(10) << name << setw(10) << amount << setw(10) << price << endl;
+	}
+
+	// 如果mode为0，输出题头，否则不输出题头
+}
+
+void Medicine::swap(Medicine* a, Medicine* b)
+{
+	Medicine * p = NULL;
+	a = p;
+	p = b;
+	b = a;
+}
+
+void Medicine::Delete()
+{
+	accessibility = false;
+	total_index[index] = NULL;
+	for (int i = 0; i < count; i++)
+	{
+		if (total_index[i] == NULL)
+		{
+			for (int j = i; j < count; j++)
+			{
+				swap(total_index[j], total_index[j + 1]);
+			}
+		}
+	}			// 保证total_index里面null都在后面，这样下面的count--才能正常工作
+				// 这一段亟待修改，这样做速度太慢了
+	count--;
+}
+
+void Medicine::showAllMedicine()
+{
+	for (int i = 0; i < count; i++)
+	{
+		if (total_index[i] == NULL)
+		{
+			break;
+		}
+		cout << setw(10) << "NO" << setw(10) << "NAME" << setw(10) << "AMOUNT" << setw(10) << "PRICE" << endl;
+		(*total_index)->show(1);
+	}
+}
---
+++ medicine_management_system/medicine.h
@@ -0,0 +1,35 @@
+// header file of class Medicine
+#ifndef MEDICINE_H
+#define MEDICINE_H
+#pragma once
+
+#include<string>
+
+const int MAX_RECORD = 1000;
+
+using std::string;
+
+class Medicine
+{
+private:
+	string name;
+	int number;
+	int amount;
+	double price;
+	bool accessibility;
+	int index;
+	static int count;
+	static Medicine **total_index;
+
+public:
+	Medicine() {};
+	Medicine(int t_number, string t_name, int amount, double price);
+	Medicine(Medicine & t_medicine);
+	void add(int t_number, string t_name, int t_amount, double t_price);
+	void show(int mode = 0);
+	void Delete();
+	static void showAllMedicine();
+	static void swap(Medicine* a, Medicine* b);
+};
+
+#endif
